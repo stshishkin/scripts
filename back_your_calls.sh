8af38110fe022e9d5aaad5ebcfb5fadfcf4e35e7
stshishkin <st.shishkin@gmail.com>
2016-08-14 18:40:46 +0300

    copy record files script
    This script copy files from common directory to personal client's destination according to database information. It's useful for recovering call records that were mistakenly deleted.

---
+++ back_your_calls.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+UNAME=$1
+SRC_DIR="/home/cb_asterisk/trash/"
+DST_DIR="/home/cb_asterisk/include/clients/$UNAME/call_records"
+
+CNT_FILES=$(mysql -u$MYSQL_USER -p$MYSQL_PASS cb_asterisk -BN -e"select count(record_fname) from logger where uname='$UNAME' and duration>0")
+CP_FILES=0
+
+for file in $( mysql -u$MYSQL_USER -p$MYSQL_PASS cb_asterisk -BN -e"select record_fname from logger where uname='$UNAME' and duration>0" ); do
+	if cp $SRC_DIR${file##*/} $DST_DIR; then
+		printf "($((CP_FILES++))/$CNT_FILES) copied           \r"
+	fi	
+done
+echo "I've copied $CP_FILES files from $CNT_FILES metioned in database"
